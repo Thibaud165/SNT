69e492d8a0e0e96da324f2c26da757ab21df6d0f
Thibaud Jourdan <thibaudj165@gmail.com>
2026-05-11 10:53:22 +0000

    tableur et texte

--- activite1.py
+++ activite1.py
@@ -1,52 +1,24 @@
-import calendar
 import datetime
 
+maintenant = datetime.datetime.now()
+annee_actuelle = maintenant.year
+mois_actuel = maintenant.month
+jour_actuel = maintenant.day
 
-def calculer_age(date_naissance, date_actuelle):
-  if date_naissance > date_actuelle:
-    return None
+annee_naissance = int(input("Année de naissance : "))
+mois_naissance = int(input("Mois de naissance : "))
+jour_naissance = int(input("Jour de naissance : "))
 
-  annees = date_actuelle.year - date_naissance.year
-  mois = date_actuelle.month - date_naissance.month
-  jours = date_actuelle.day - date_naissance.day
+ans = annee_actuelle - annee_naissance
+mois = mois_actuel - mois_naissance
+jours = jour_actuel - jour_naissance
 
-  if jours < 0:
-    mois_precedent = date_actuelle.month - 1 or 12
-    annee_precedente = date_actuelle.year if date_actuelle.month > 1 else date_actuelle.year - 1
-    jours += calendar.monthrange(annee_precedente, mois_precedent)[1]
+if jours < 0:
+    jours += 30
     mois -= 1
 
-  if mois < 0:
+if mois < 0:
     mois += 12
-    annees -= 1
+    ans -= 1
 
-  return annees, mois, jours
-
-
-print("Votre date de naissance (jour/mois/annee) :")
-naissance_complete = input(">>> ")
-print()
-
-try:
-  naissance_date = datetime.datetime.strptime(naissance_complete, "%d/%m/%Y").date()
-except ValueError:
-  print("Erreur, format de date invalide")
-else:
-  actuelle_date = datetime.date.today()
-  actuelle_complete = actuelle_date.strftime("%d/%m/%Y")
-
-  print("Date actuelle (jour/mois/annee) :")
-  print(">>>", actuelle_complete)
-  print()
-
-  age = calculer_age(naissance_date, actuelle_date)
-
-  if age is None:
-    print("Erreur, la date de naissance est dans le futur")
-  else:
-    age_annee, age_mois, age_jour = age
-    print("Vous avez :")
-    print(age_annee, "ans")
-    print(age_mois, "mois")
-    print(age_jour, "jours")
-    print()
+print(f"Votre âge : {ans} ans, {mois} mois et {jours} jours.")
